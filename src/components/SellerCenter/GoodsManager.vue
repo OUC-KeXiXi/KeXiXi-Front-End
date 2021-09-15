0dcd8f8e460360ff06e7c84fb4e231197d558127
河童 <893387721@qq.com>
2021-09-15 11:35:58 +0800

    feat: 卖家中心部分内容和组件通信优化

---
+++ src/components/SellerCenter/GoodsManager.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="goodsmanager">
+    <el-tabs v-model="activeName" type="card" @tab-click="handleSwitchTab">
+      <el-tab-pane label="已发布商品" name="first">
+        <el-card class="tab-card">
+          <GoodsList :published="true"/>
+        </el-card>
+      </el-tab-pane>
+      <el-tab-pane label="商品草稿箱" name="second">
+        <el-card class="tab-card">
+          <GoodsList :published="false"/>
+        </el-card>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<script>
+import GoodsList from "../SellerCenter/GoodsList.vue"
+export default {
+  name: "GoodsManager",
+  components:{
+    GoodsList
+  },
+  data() {
+    return {
+      activeName: "first",
+    };
+  },
+  created() {},
+  methods: {
+    handleSwitchTab(tab, event) {
+      console.log(tab, event);
+    },
+    toPublish(){
+
+    }
+  },
+};
+</script>
+
+<style scoped lang="less">
+@import "../../style/style.less";
+
+</style>
---
+++ src/components/SellerCenter/PublishDialog.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-dialog v-model="publishDialogVisible" :title="formTitle">
+    <el-form
+      :model="publishForm"
+      ref="publishForm"
+      :rules="publishRules"
+      status-icon
+      hide-required-asterisk
+      label-position="right"
+      label-width="100px"
+    >
+      <el-form-item label="用户名/邮箱" prop="title">
+        <el-input v-model="publishForm.title" />
+      </el-form-item>
+      <el-form-item label="密码" prop="content">
+        <el-input v-model="publishForm.content" />
+      </el-form-item>
+      <div class="dialog-footer">
+        <el-button @click="publishDialogVisible = false">取消</el-button>
+        <el-button type="primary">发布</el-button>
+      </div>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: "PublishDialog",
+  props: {
+    course: Object,
+  },
+  data() {
+    return {
+      publishDialogVisible: false,
+      formTitle:"发布商品",
+      publishForm: {
+        title: this.course.title,
+        content: this.course.content,
+        cover: this.course.cover,
+        price: this.course.price,
+        tags: this.course.tags,
+      },
+      publishrules: {},
+    };
+  },
+  created() {},
+  methods: {
+    openDialog() {
+      this.publishDialogVisible = true;
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+@import "../../style/style.less";
+.dialog-footer {
+  display: flex;
+  justify-content: flex-end;
+}
+.dialog-footer > .el-button {
+  margin: 0 1%;
+}
+
+</style>
